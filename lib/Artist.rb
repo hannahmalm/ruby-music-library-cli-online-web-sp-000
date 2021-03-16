f088dc04a38eecd05d4c9e4b9f291e79e45e5417
Hannah Malm <hannahemalm616@gmail.com>
2021-03-16 01:18:22 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,21 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name=name 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  def save 
+    self.class.all << self 
+  end 
+
+end 
